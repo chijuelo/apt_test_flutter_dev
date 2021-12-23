c5c8c78eea40dbfaff75de5e6c28324dc66511af
Ryuuzaki <chijuelo89@gmail.com>
2021-12-22 19:51:23 -0500

    Update home.dart

--- lib/widgets/home.dart
+++ lib/widgets/home.dart
@@ -1,5 +1,7 @@
 import 'package:apt_test_flutter_dev/shared/colors.dart';
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:google_nav_bar/google_nav_bar.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -9,80 +11,60 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  final _scaffoldKey = GlobalKey<ScaffoldState>();
+  int _selectedIndex = 0;
+
+  final List<Widget> tabs = [
+    // PortfolioSection(),
+    // MarketsSection(),
+    // SearchSection(),
+    // NewsSection(),
+  ];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          centerTitle: true,
-          backgroundColor: const Color.fromRGBO(241, 241, 241, 1),
-          elevation: 0.0,
-          toolbarHeight: 60,
-          leading: IconButton(
-              icon: const Icon(
-                Icons.menu,
-                size: 30,
-                color: Colors.black,
-              ),
-              onPressed: () => _scaffoldKey.currentState?.openDrawer()),
-          actions: [
-            Padding(
-              padding: const EdgeInsets.all(4.0),
-              child: Container(
-                padding: const EdgeInsets.all(5.0),
-                decoration: BoxDecoration(
-                    border: Border.all(color: Colors.grey),
-                    color: Colors.white,
-                    borderRadius: const BorderRadius.all(Radius.circular(80))),
-                child: ClipRRect(
-                    borderRadius: BorderRadius.circular(100.0),
-                    child: Container(
-                      height: 40.0,
-                      width: 40.0,
-                      color: Colors.black,
-                      // child: const FadeInImage(
-                      //   image: AssetImage('assets/images/avatar.png'),
-                      //   placeholder: AssetImage('assets/images/avatar.png'),
-                      //   fadeInDuration: Duration(milliseconds: 200),
-                      //   fit: BoxFit.fill,
-                      // )
-                    )),
-              ),
-            ),
-            const SizedBox(width: 5)
-          ],
-        ),
         backgroundColor: kScaffoldBackground,
-        key: _scaffoldKey,
-        floatingActionButton: FloatingActionButton(
-            onPressed: () {}, child: const Icon(Icons.add)),
-        body: ListView(padding: const EdgeInsets.all(15.0), children: [
-          Row(
-            children: const [
-              Text(
-                'Hola, ',
-                style: TextStyle(fontSize: 30, fontFamily: 'Roboto'),
-              ),
-              Text(
-                'Julio',
-                style: TextStyle(
-                    fontSize: 30, fontFamily: 'Roboto', color: Colors.blue),
-              )
-            ],
+        body: tabs.elementAt(_selectedIndex),
+        bottomNavigationBar: SafeArea(
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 18.0, vertical: 8),
+            child: GNav(
+                gap: 8,
+                activeColor: Colors.white,
+                iconSize: 24,
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 20, vertical: 5),
+                duration: const Duration(milliseconds: 800),
+                tabBackgroundColor: Colors.white30,
+                selectedIndex: _selectedIndex,
+                tabs: _bottomNavigationBarItemItems(),
+                onTabChange: _onItemTapped),
           ),
-          const SizedBox(height: 5),
-          Row(
-            children: const [
-              Text('Qué quieres buscar...?',
-                  style: TextStyle(fontSize: 20, fontFamily: 'Roboto')),
-            ],
-          ),
-          const SizedBox(height: 5),
-          Row(
-            children: const [],
-          ),
-          const SizedBox(height: 10),
-        ]));
+        ));
+  }
+
+  List<GButton> _bottomNavigationBarItemItems() {
+    return [
+      const GButton(
+        icon: FontAwesomeIcons.shapes,
+        text: 'Home',
+      ),
+      const GButton(
+        icon: FontAwesomeIcons.suitcase,
+        text: 'Markets',
+      ),
+      const GButton(
+        icon: FontAwesomeIcons.search,
+        text: 'Search',
+      ),
+      const GButton(
+        icon: FontAwesomeIcons.globeAmericas,
+        text: 'News',
+      ),
+    ];
+  }
+
+  void _onItemTapped(int index) {
+    setState(() => _selectedIndex = index);
   }
 }
